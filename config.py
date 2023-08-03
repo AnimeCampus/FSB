bfff39d15fc4822c468cfafbfb600cabd5c3c9d2
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-03 12:03:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://bqzfecbk:HM7GIMcO-5-8NSxyQL-aT_SRF9GbFOWc@arjuna.db.elephantsql.com/bqzfecbk")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001800560661"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001941708305"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0")) 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
